de21dc0d27ca6200e8cc2ab48fcba57d52bc68f3
Cristian Bote <cristian.bote@paddypowerbetfair.com>
2017-06-27 09:11:52 +0300

    Get rid of workerbox

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,43 @@
-importScripts('https://unpkg.com/workbox-sw@1.0.1');
+const CACHE_NAME = '1-vanilla_hn';
+const URLS = [
+    '/',
+    '/index.html',
+    '/lib/firebase.js',
+    '/icons/192x192.png',
+    '/icons/384x384.png',
+    '/icons/512x512.png',
+    '/favicon.png',
+    '/manifest.json',
+    '/bundle.js'
+];
 
-const workboxSW = new WorkboxSW({clientsClaim: true});
+// Respond with cached resources
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (request) {
+            return request || fetch(event.request)
+        })
+    );
+});
 
-// This array will be populated by workboxBuild.injectManifest() when the
-// production service worker is generated.
-workboxSW.precache([]);
+// Cache resources
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(URLS)
+        })
+    );
+});
 
-workboxSW.router.setDefaultHandler({
-    handler: workboxSW.strategies.staleWhileRevalidate()
+const clearOldCaches = () => {
+    return caches.keys().then(keys => {
+        return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+    });
+};
+
+// Delete outdated caches
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        clearOldCaches().then(() => self.clients.claim())
+    );
 });
