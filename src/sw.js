e70babc4af520106352c01026d1b38b0fd162ad4
Cristian Bote <cristian.bote@paddypowerbetfair.com>
2017-07-04 10:55:08 +0300

    Stupid sw config

--- src/sw.js
+++ src/sw.js
@@ -3,14 +3,6 @@ const URLS = [
     '/',
     '/index.html',
     '/lib/firebase.js',
-    '/icons/icon-72x72.png',
-    '/icons/icon-96x96.png',
-    '/icons/icon-128x128.png',
-    '/icons/icon-144x144.png',
-    '/icons/icon-152x152.png',
-    '/icons/icon-192x192.png',
-    '/icons/icon-384x384.png',
-    '/icons/icon-512x512.png',
     '/favicon.ico',
     '/bundle.js'
 ];
