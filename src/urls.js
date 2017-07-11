55520eeaddf0111c44619482243bc3902e0177a6
Cristian Bote <cristian.bote@paddypowerbetfair.com>
2017-07-11 22:59:58 +0300

    Trying out hnpwa.com

--- src/urls.js
+++ src/urls.js
@@ -1,8 +1,8 @@
 export const urls = {
-    'newstories': (page) => `https://node-hnapi.herokuapp.com/newest?page=${page}`,
-    'topstories': (page) => `https://node-hnapi.herokuapp.com/news?page=${page}`,
-    'askstories': (page) => `https://node-hnapi.herokuapp.com/ask?page=${page}`,
-    'jobstories': (page) => `https://node-hnapi.herokuapp.com/jobs?page=${page}`,
-    'showstories': (page) => `https://node-hnapi.herokuapp.com/show?page=${page}`,
-    'item': (id) => `https://node-hnapi.herokuapp.com/item/${id}`
+    'newstories': (page) => `https://hnpwa.com/api/v0/newest.json?page=${page}`,
+    'topstories': (page) => `https://hnpwa.com/api/v0/news.json?page=${page}`,
+    'askstories': (page) => `https://hnpwa.com/api/v0/ask.json?page=${page}`,
+    'jobstories': (page) => `https://hnpwa.com/api/v0/jobs.json?page=${page}`,
+    'showstories': (page) => `https://hnpwa.com/api/v0/show.json?page=${page}`,
+    'item': (id) => `https://hnpwa.com/api/v0/item/${id}.json`
 };
