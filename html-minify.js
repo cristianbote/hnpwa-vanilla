34544d698d4d5c290cc689b9bceae020e64e4cd4
Cristian Bote <cristian.bote@paddypowerbetfair.com>
2017-11-01 23:05:36 +0200

    Add #6 and version in header

--- html-minify.js
+++ html-minify.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const version = require('./package.json').version;
 const FILE_PATH = './public/index.html';
 const DESTINATION_PATH = './public/index.html';
 const minify = require('html-minifier').minify;
@@ -15,6 +16,8 @@ fileData = minify(fileData, {
     collapseWhitespace: true,
     preserveLineBreaks: false
 });
+
+fileData = fileData.replace('{{version}}', version);
 
 console.log('[html-minify] write file', DESTINATION_PATH, ((1 - fileData.length / size) * 100).toFixed(2));
 fs.writeFileSync(DESTINATION_PATH, fileData, 'utf8');
